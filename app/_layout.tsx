23b0e610ce7b6c819c911853955d55104b0caa94
abhi210agni <abhishek.tyagi@agniverse.co>
2025-10-28 00:01:01 +0530

    add protected routes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,29 @@
+import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import { Stack } from "expo-router";
+import { ActivityIndicator } from "react-native";
 
+function RootLayoutWithAuth() {
+  const { isSignedIn, isLoaded } = useAuth();
+  if (!isLoaded) return <ActivityIndicator />;
+  return (
+    <Stack>
+      <Stack.Protected guard={isSignedIn}>
+        <Stack.Screen name="(protected)" options={{ headerShown: false }} />
+      </Stack.Protected>
+      <Stack.Protected guard={!isSignedIn}>
+        <Stack.Screen name="(public)" options={{ headerShown: false }} />
+      </Stack.Protected>
+    </Stack>
+  );
+}
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ClerkProvider
+      tokenCache={tokenCache}
+      publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <RootLayoutWithAuth />
+    </ClerkProvider>
+  );
 }
